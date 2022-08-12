4c31a3f565b3a2a1d5def931a40fbd5e7c0f09e9
Lisa Swart <lisswart@amazon.com>
2022-08-11 21:20:37 -0400

    add pedac process for double_numbers.rb

---
+++ double_numbers.rb
@@ -0,0 +1,43 @@
+# INPUT: Array
+# OUTPUT: Array
+
+#1 Restate the problem
+#2 Understand the examples and test cases
+#3 Formulate the goal
+
+# PROBLEM: You are given an array of integers. You are
+# tasked with transforming each integer in input array
+# into twice its original numeric value.
+
+# EXAMPLES / TEST CASES:
+# double_numbers([1, 2, 3]) == [2, 4, 6]
+# double_numbers([]) == 
+
+# DATA STRUCTURE:
+# Array -> Array
+
+# ALGORITHM:
+# - Iterate over each integer in input array
+# - multiply current integer by 2
+# - push transformed element into a new array `doubled_numbers`
+# - return `doubled_numbers`
+
+# CODING WITH INTENT
+def double_numbers(numbers)
+    doubled_numbers = []
+    counter = 0
+
+    loop do
+        break if counter == numbers.size
+
+        current_number = numbers[counter]
+        doubled_numbers << current_number * 2
+
+        counter += 1
+    end
+
+    doubled_numbers
+end
+
+puts double_numbers([1, 2, 3]) == [2, 4, 6]
+puts double_numbers([]) == []
